40df30a2f9b4cb9cdf54ad2d05fc18d2a4ed7e47
ben-goldberg <ben.goldberg@originate.com>
2016-05-08 18:41:44 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+var SlackBot = require('slackbots');
+
+// create a bot
+var lunchbot = new SlackBot({
+    token: process.env.BOT_API_KEY,
+    name: 'Lunchbot'
+});
+
+function getUserById(bot, id) {
+  return bot.users.filter(function(user) {
+    return user.id === id;
+  })[0]
+}
+
+lunchbot.on('start', function() {
+  console.log('starting up!')
+});
+
+lunchbot.on('message', function(data) {
+  if(data.type == 'message' && data.subtype != 'bot_message') {
+    console.log(data)
+    var user = getUserById(lunchbot, data.user);
+    lunchbot.postMessageToUser(user.name, 'Hello to you too!');
+  }
+});
